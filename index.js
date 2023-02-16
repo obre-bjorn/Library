0ad34b741ccd3c50b19bafbe191eb63eef138081
obre-bjorn <obre.bjorn@gmail.com>
2023-02-16 12:24:06 +0300

    delete Book

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function Book(title, author, pages, cover = "", read = false) {
 
 
 //  ADD BOOK BUTTON
+
 let addBookForm = document.querySelector('#addBookForm');
 
 
@@ -36,9 +37,23 @@ addBookForm.addEventListener('submit', (e) => {
     addBookForm.reset();
 
 
+    let deleteBtn = document.querySelector('.deleteBook')
+
+    deleteBtn.addEventListener('click', deleteBook);
+
     toggleModal()
 
 })
+
+
+// MODAL TOGGLER
+
+function toggleModal() {
+    modal.classList.toggle('flex');
+    modal.classList.toggle('hidden')
+    return
+}
+
 
 //  SHOW MODAL
 
@@ -46,7 +61,8 @@ addBookBtn.addEventListener('click', () => {
     toggleModal()
 })
 
-// MODAL  FUNCTIONALITY
+// MODAL  
+
 let formContainer = document.querySelector('.addbookFormContainer')
 
 formContainer.addEventListener('click', (e) => {
@@ -60,24 +76,19 @@ modal.addEventListener('click', (e) => {
 })
 
 
-// MODAL TOGGLER
-function toggleModal() {
-    modal.classList.toggle('flex');
-    modal.classList.toggle('hidden')
-    return
-}
 
 
 
 // ADD BOOK TO DOM
+
 function AddBook(book, index) {
     let bookContainer = document.querySelector('#bookContainer');
 
 
     bookContainer.innerHTML += `
-           <div id="${index}" class="bookCard w-96 bg-green-300 rounded-xl p-10 text-center shadow-xl flex flex-col items-center gap-5 transition-all hover:scale-105">
-                <button class=" deleteBook bg-red-700 text-white px-2 py-0.5 self-end rounded-2xl hover:bg-red-600 border-none shadow-3xl hover:scale-125  transition-all delay-150">X</button>
-                <img class="mx-auto mt-2 w-1/2  rounded-xl hover:rounded-none transition-all delay-200 " src="${book.cover}"
+           <div id="${index}" class="bookCard w-80 bg-green-300 rounded-xl p-10 text-center shadow-xl flex flex-col items-center gap-5 transition-all hover:scale-105">
+                <button class="deleteBook bg-red-700 text-white px-2 py-0.5 self-end rounded-2xl hover:bg-red-600 border-none shadow-3xl hover:scale-125 hover:rotate-90 transition-all delay-150">X</button>
+                <img class="mx-auto mt-2 w-32  rounded-xl hover:rounded-none transition-all delay-200 " src="${book.cover}"
                     alt="${book.title}">
                 <p class="bookTitle text-2xl font-extrabold text-purple-800">${book.title}</p>
                 <p class="author text-md font-light ">By: ${book.author}</p>
@@ -86,26 +97,13 @@ function AddBook(book, index) {
             </div>
         `
 
-    console.log(book)
-        // console.log(bookContainer);
+    console.log(book);
 
+    // console.log(bookContainer);
 }
 
+// DELETE BOOK FROM LIBRARY & DOM
 
-
-// deleteBooksBtn.forEach((btn) => {
-//     btn.addEventListener('click', function(e) {
-//         let bookContainer = document.
-//         console.log(this.parentNode)
-//         console.log(e.target.parentNode)
-//     })
-// })
-
-// console.log(deleteBooksBtn);
-const Book1 = new Book('Way of Men', 'Andrew Kibe', 350, true);
-const Book2 = new Book('This is Africa', 'Abel Mutua', 280);
-const Book3 = new Book('Rich Dad Poor Dad', 'Rick Kwoloski', 250);
-// addBookToLibrary(Book1)
-// addBookToLibrary(Book2)
-// addBookToLibrary(Book3)
-// displayBooks(myLibrary)
+function deleteBook(e) {
+    console.log(this.parentNode.parentNode)
+}
