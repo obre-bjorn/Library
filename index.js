2e0c349d6035ebeaa4054a5e9897bde712dbd876
obre-bjorn <obre.bjorn@gmail.com>
2023-04-02 23:40:32 +0300

    classes for book object

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ let addBookBtn = document.querySelector('.addBook');
 let modal = document.querySelector('.modal');
 
 
-function Book(title, author, pages, cover = "", read = false) {
-    //constructor
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.cover = cover;
-    this.read = read;
+// function Book(title, author, pages, cover = "", read = false) {
+//     //constructor
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.cover = cover;
+//     this.read = read;
+
+// }
+
+class Book {
+    constructor(title, author, pages, cover = "", read = false) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.cover = cover;
+        this.read = read;
+    }
+
 
 }
 
@@ -17,9 +29,16 @@ function Book(title, author, pages, cover = "", read = false) {
 //  ADD BOOK BUTTON
 
 let addBookForm = document.querySelector('#addBookForm');
-
-
 addBookForm.addEventListener('submit', addBook)
+
+
+// MODAL  
+
+let formContainer = document.querySelector('.addbookFormContainer')
+
+formContainer.addEventListener('click', (e) => {
+    e.stopPropagation()
+})
 
 
 
@@ -32,20 +51,12 @@ function toggleModal() {
 }
 
 
-//  SHOW MODAL
+
+//  TOGGLE MODAL
 
 addBookBtn.addEventListener('click', () => {
     toggleModal()
 })
-
-// MODAL  
-
-let formContainer = document.querySelector('.addbookFormContainer')
-
-formContainer.addEventListener('click', (e) => {
-    e.stopPropagation()
-})
-
 modal.addEventListener('click', (e) => {
 
     toggleModal()
@@ -68,22 +79,15 @@ function addBook(e) {
     const book = new Book(formProps['book-title'], formProps['book-author'], formProps['book-pages'], formProps['book-cover']);
     myLibrary.push(book);
 
-
     let libraryIndex = myLibrary.length - 1
 
-    console.log(libraryIndex)
     addBookForm.reset();
-
-
 
     toggleModal()
 
-
-
     let bookContainer = document.querySelector('#bookContainer');
-
-
-    bookContainer.insertAdjacentHTML("afterbegin", `
+    bookContainer.insertAdjacentHTML("afterbegin",
+        `
            <div class="bookCard w-80 bg-green-300 rounded-xl p-10 text-center shadow-xl flex flex-col items-center gap-5 transition-all hover:scale-105">
                 <button id ="remove-book-${libraryIndex}" class="deleteBook bg-red-700 text-white px-2 py-0.5 self-end rounded-2xl hover:bg-red-600 border-none shadow-3xl hover:scale-125 hover:rotate-90 transition-all delay-150">X</button>
                 <img class="mx-auto mt-2 w-32  rounded-xl hover:rounded-none transition-all delay-200 " src="${book.cover}"
@@ -93,18 +97,20 @@ function addBook(e) {
                 <p class="page ">Pages: ${book.pages}</p>
                 <p class="read ">Read: ${book.read}</p>
                 </div>
-                `)
+                `
+    )
+
+
+
+    // DELETE BOOK FROM LIBRARY & DOM
 
     document.querySelector('#remove-book-' + libraryIndex)
         .addEventListener('click', function() {
             let parent = this.parentNode.parentNode
             let child = this.parentNode
-            console.log(this)
 
             parent.removeChild(child)
         });
 
     // console.log(bookContainer);
 }
-
-// DELETE BOOK FROM LIBRARY & DOM
